ac7d171ff09c7e6e14ce049fbddfd6c6918c0a35
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-05 20:26:59 -0500

    Started working on user profile page (user "show" page)

--- views/users/account.php
+++ views/users/account.php
@@ -1,1 +1,15 @@
 <!--Page for user account home-->
+<div class="container">
+	<div class="row">
+
+		<h1 class="text-center">User Information</h1>
+		<p class="text-center">Name: </p>
+		<p class="text-center">Username: </p>
+		<p class="text-center">Email:</p>
+
+		<div class="col-sm-12 text-center">
+		    <a href="/users/account/edit?id=1" class="btn btn-primary"><i class="fa fa-pencil"></i> Edit Profile</a>
+		</div>
+
+	</div>
+</div>
