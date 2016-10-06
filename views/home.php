754a972c69725730a1721217603cf3964503d76f
Liz Peacock <liz.peacock11@gmail.com>
2016-10-06 15:34:56 -0500

    fixed indentation

--- views/home.php
+++ views/home.php
@@ -11,9 +11,9 @@
                     <h1>Welcome to Kitchen Kingdom!</h1>
 
                     <p>Your source for all the unique kitchen gadgets you never knew you needed</p>
-                
+
                 </div>
-            
+
             </div>
 
         </div>
@@ -27,55 +27,34 @@
 <div class="container">
 
     <section id="features">
-    
-    <div class="features-head">
 
-        <h3>Featured Items</h3>
-        
-    </div>
+        <div class="features-head">
 
-    <!-- featured items row -->
+            <h3>Featured Items</h3>
 
-       <div class="row">
-       
-       <?php foreach ($features as $feature) : ?>
-           
-           <div class="col-sm-4">
-               
-               <img class="image-responsive" src="http://placehold.it/225x225">
-               
-               <h4><?= $feature['product_name'] ?></h4>
+        </div>
 
-               <p class="featured"><?= $feature['description'] ?></p>
-           
-           </div>
-      <?php endforeach; ?>
+        <!-- featured items row -->
 
-           <!-- <div class="col-sm-4">
-               
-               <img class="image-responsive" src="http://placehold.it/225x225">
-               
-               <h4>Item Name</h4>
-               
-               <p class="featured">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-               tempor incididunt ut labore et dolore magna aliqua.</p>
-           
-           </div>
+        <div class="row">
 
-           <div class="col-sm-4">
-               
-               <img class="image-responsive" src="http://placehold.it/225x225">
-               
-               <h4>Item Name</h4>
-               
-               <p class="featured">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-               tempor incididunt ut labore et dolore magna aliqua.</p>
-           
-           </div> -->
+            <?php foreach ($features as $feature) : ?>
 
-       </div>
-       <!-- close featured items row -->
-   </section>
-   <!-- close features section -->
+                <div class="col-sm-4">
+
+                    <img class="image-responsive" src="http://placehold.it/225x225">
+
+                    <h4><?= $feature['product_name'] ?></h4>
+
+                    <p class="featured"><?= $feature['description'] ?></p>
+
+                </div>
+
+            <?php endforeach; ?>
+
+        </div>
+        <!-- close featured items row -->
+    </section>
+    <!-- close features section -->
 </div>
 <!-- close features container -->
