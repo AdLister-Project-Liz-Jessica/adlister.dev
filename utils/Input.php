a65ffe3af1187e363992022c892efc57f510890f
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-08 10:38:19 -0500

    Added methods to Input.php to make sure the username / email had not already been used and that it has the right string length. Also added try catches to username and email properties. Sign up page is fully functioning.

--- utils/Input.php
+++ utils/Input.php
@@ -23,14 +23,45 @@ class Input
     {
         if(isset($_REQUEST[$key])) {
             return $_REQUEST[$key];
+
         } else {
             return $default;
         }
-        //my edits for exceptions
-        if(User::findByUsernameOrEmail($username_or_email)){
-            throw new UnexpectedValueException("Value '$username_or_email' is already taken. Please try another $username_or_email");
-        }
+
     }
+
+
+    public static function getUsername($key, $min = 4, $max = 20){
+
+        $value = self::get($key);
+
+        if(strlen($value)<$min|| strlen($value)>$max){
+            throw new LengthException("Username must be between '$min' and '$max' characters long");
+        }
+
+        if(User::findByUsernameOrEmail($value)){
+            throw new UnexpectedValueException("Username '$value' is already taken. Please try another username.");
+        }
+
+        return $value;
+
+    }
+
+    public static function getEmail($key, $min = 5, $max = 50){
+
+        $value = self::get($key);
+
+        if(strlen($value)<$min || strlen($value)>$max){
+            throw new LengthException("Email '$value' is already taken. Please try another '$value' ");
+        }
+
+        if(User::findByUsernameOrEmail($value)){
+            throw new UnexpectedValueException("Email '$value' is already taken. Please try another email.");
+        }
+
+        return $value;
+    }
+
 
     // returns entire array from request super global
     public static function all()
--- views/users/signup.php
+++ views/users/signup.php
@@ -1,10 +1,9 @@
 <?php 
 	//if there is something in all the inputs
 	$message = "Please fill out the following to create your account:";
-	$error = "";
+	$errors = [];
 	if(Input::get('name') && Input::get('username') && Input::get('email') && Input::get('password')){
 
-		$errors = [];
 
 		if(!empty($_POST)){
 
@@ -12,27 +11,37 @@
 
 			//add try catch if user tries to sign up with an already taken username
 			try{
-				$username = Input::get('username');
+				$username = Input::getUsername('username');
 			}catch(UnexpectedValueException $e){
+				$errors["username"] = $e->getMessage();
+			}catch(LengthException $e){
 				$errors["username"] = $e->getMessage();
 			}
 			//add try catch if user tries to sign up with an already taken email
+
+
 			try{
-				$email = Input::get('email');
+				$email = Input::getEmail('email');
 			}catch(UnexpectedValueException $e){
+				$errors["email"] = $e->getMessage();
+			}catch(LengthException $e){
 				$errors["email"] = $e->getMessage();
 			}
 
 			$password = Input::get('password');
-			$newUser = new User();
-			$newUser->name = $name;
-			$newUser->username = $username;
-			$newUser->email = $email;
-			$newUser->password = $password;
 			
-			$newUser->save();
+			if(empty($errors)){
+				$newUser = new User();
+				$newUser->name = $name;
+				$newUser->username = $username;
+				$newUser->email = $email;
+				$newUser->password= $password;
+				
+				$newUser->save();
 
-		$message = "Sign up Succesful! Please log in.";
+
+				$message = "Sign up Succesful! Please log in.";
+			}
 
 		}else{
 		 $message = "Try Again";
@@ -67,10 +76,13 @@
 	                <?php unset($_SESSION['SUCCESS_MESSAGE']); ?>
 	            <?php endif; ?>
 
+	     <ul>
 
+	    <?php foreach ($errors as $error){ ?>
  		
- 		<?php echo $error .PHP_EOL;?>
+ 				<li><?php echo $error .PHP_EOL;} ?></li>
  		
+ 		</ul>
 
 				<form method="POST" action="" data-validation data-required-message="This field is required">
 				<h4> <?= $message ?> </h4>
