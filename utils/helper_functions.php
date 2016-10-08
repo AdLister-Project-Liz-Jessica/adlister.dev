c0349dbdeec6b3004d7d1d72471eb53c447d4e79
Liz Peacock <liz.peacock11@gmail.com>
2016-10-08 17:26:24 -0500

    added a bunch of var_dumps/echos for testing

--- utils/helper_functions.php
+++ utils/helper_functions.php
@@ -7,9 +7,13 @@
 function saveUploadedImage($input_name)
 {
     $valid = true;
+    var_dump($valid);
     // checks if $input_name is in the files super global
+    echo (isset($_FILES[$input_name]));
+    echo ($_FILES[$input_name]['name']);
     if(isset($_FILES[$input_name]) && $_FILES[$input_name]['name'])
     {
+        var_dump($_FILES);
         // checks if there are any errors on the upload from the submission
         if(!$_FILES[$input_name]['error'])
         {
@@ -17,17 +21,20 @@ function saveUploadedImage($input_name)
             $positionOfLastSlash = strrpos($tempFile, '/');
             $newName = substr($tempFile, $positionOfLastSlash);
             $extension = pathinfo($_FILES[$input_name]['name'], PATHINFO_EXTENSION);
+            var_dump($_FILES);
             
             // Validate Size and Extension
             if( $_FILES[$input_name]['size'] > (1024000000))
             {
                 $valid = false;
+                var_dump($valid);
             }
             
             // only allows certain file extensions
             if( $extension != 'jpg' && $extension != 'jpeg' && $extension != 'png' && $extension != 'gif')
             {
                 $valid  = false;
+                var_dump($valid);
             }
             
             // If Image file makes it to this point, send file to this directory
@@ -35,6 +42,7 @@ function saveUploadedImage($input_name)
             {
                 $image_url = '/img/uploads' . $newName . '.' . $extension;
                 move_uploaded_file($tempFile, __DIR__ .'/../public' . $image_url);
+                var_dump($image_url);
                 return $image_url;
 
             } else {
