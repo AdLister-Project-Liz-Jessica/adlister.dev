5315c0ed7195287abdf680cb37efcae9151a2144
Liz Peacock <liz.peacock11@gmail.com>
2016-10-09 17:24:04 -0500

    tabs to spaces

--- views/ads/show.php
+++ views/ads/show.php
@@ -3,45 +3,45 @@
 
 <?php foreach ($products as $product) : ?>
 
-	<div class="show col-sm-8">
+    <div class="show col-sm-8">
 
-		<div class="imageHolder">
+        <div class="imageHolder">
 
-			<img class="show-item-img" src="<?= $product['image_url'] ?>">
+            <img class="show-item-img" src="<?= $product['image_url'] ?>">
 
-		</div>
+        </div>
 
-		<h4 class="show-item-header"><?= $product['product_name'] ?></h4>
+        <h4 class="show-item-header"><?= $product['product_name'] ?></h4>
 
-		<p><?= $product ['description'] ?></p>
+        <p><?= $product ['description'] ?></p>
 
-	</div>
+    </div>
 
-	<div class="show col-sm-4">
+    <div class="show col-sm-4">
 
-		<p><span class="bold">Price:</span> $ <?= $product ['price'] ?></p>
-		
-		<p><span class="bold">Contact:</span> <?= $products->getUserInfo()->username ?></p>
+        <p><span class="bold">Price:</span> $ <?= $product ['price'] ?></p>
+        
+        <p><span class="bold">Contact:</span> <?= $products->getUserInfo()->username ?></p>
 
-		<p><span class="bold">Posted By:</span> <?= $products->getUserInfo()->name ?></p>
+        <p><span class="bold">Posted By:</span> <?= $products->getUserInfo()->name ?></p>
 
-	</div>
+    </div>
 
 <?php endforeach ?>
 
-	<div class="row">
-	
-		<div class="col-sm-6 button-center">
+    <div class="row">
+    
+        <div class="col-sm-6 button-center">
 
-			<button class="btn">edit ad</button>
+            <button class="btn">edit ad</button>
 
-		</div>
+        </div>
 
-		<div class="col-sm-6 button-center">
+        <div class="col-sm-6 button-center">
 
-			<button class="btn">delete ad</button>
+            <button class="btn">delete ad</button>
 
-		</div>
+        </div>
 
 
 
