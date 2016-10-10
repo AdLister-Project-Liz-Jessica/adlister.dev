4ef32c5a3330bb1bc87f93fc35af6bd9094458b7
Liz Peacock <liz.peacock11@gmail.com>
2016-10-09 19:55:07 -0500

    added function to Product class to get all products owned by a user

--- models/Product.php
+++ models/Product.php
@@ -6,7 +6,7 @@ class Product extends Model
 {
     protected static $table = 'products';
 
-    public static function features ()
+    public static function features()
     {
         //connect to DB
         self::dbconnect();
@@ -24,11 +24,42 @@ class Product extends Model
         return $features;
     }
 
-    public function getUserInfo () {
+    public function getUserInfo() 
+    {
 
         return User::find($this->user_id);
     }
 
+    public static function findAllAdsFromThisUser($id) 
+    {
+        //note to self: this fxn will be just like find() in the model class except id = user_id
+        //connect to DB
+        self::dbconnect();
+
+        //Create select statement using prepared statements
+        $query = 'SELECT * FROM ' . static::$table . ' WHERE user_id = :id LIMIT 3';
+
+        $stmt = self::$dbc->prepare($query);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        //Store the resultset in a variable named $result
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // The following code will set the attributes on the calling object based on the result variable's contents
+
+        $instance = null;
+
+        if ( $result )
+        {
+
+            $instance = new static;
+            $instance->attributes = $result;
+        }
+
+        return $instance;
+
+    }
 }
 
 
