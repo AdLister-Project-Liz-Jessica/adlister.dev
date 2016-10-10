216b62656b6c36cc663518e8a2c6f9f55379b595
Liz Peacock <liz.peacock11@gmail.com>
2016-10-09 23:20:10 -0500

    added cdn for fontawesome

--- views/partials/common_js.php
+++ views/partials/common_js.php
@@ -1,4 +1,6 @@
 <!--Partial view for common javascipt files-->
+<!-- fontawesome -->
+<script src="https://use.fontawesome.com/0d9255860f.js"></script>
 <!-- javascript cdn -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <!-- jquery -->
