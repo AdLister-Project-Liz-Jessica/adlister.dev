12b1756f6934a6bbf32565313795dcc334717200
Liz Peacock <liz.peacock11@gmail.com>
2016-10-09 23:20:45 -0500

    changed name

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -3,7 +3,7 @@
     <footer>
         <div class="row">
             <div class="col-lg-12">
-                <p>Copyright &copy; Kitchen Kingdom 2016</p>
+                <p>Copyright &copy; Kitchen Kweendom 2016</p>
             </div>
         </div>
     </footer>
