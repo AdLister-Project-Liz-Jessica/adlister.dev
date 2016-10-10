49b92e79ca2f321a6945322ccfe15b174e1502e6
Liz Peacock <liz.peacock11@gmail.com>
2016-10-09 23:21:08 -0500

    added google fonts

--- views/partials/head.php
+++ views/partials/head.php
@@ -3,6 +3,10 @@
 <!-- bootstrap CDN -->
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+<!-- fonts -->
+<link href="https://fonts.googleapis.com/css?family=Cormorant+Upright:400,700" rel="stylesheet">
+
+
 
 <!-- custom styling -->
 <link href="/css/main.css" rel="stylesheet">
